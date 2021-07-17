deffe26687737def137ce3829fa8c1d4976cb1c8
Erutase Akpobome <erutase.akpobome@liverpoolft.nhs.uk>
2021-07-17 21:54:39 +0100

    Added custom route to get all firstnames using get. Added comments to build documentation

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -4,7 +4,10 @@ using System.Web.Http;
 using System.Linq;
 
 namespace DemoAPI.Controllers
-{
+{ 
+    /// <summary>
+    /// This is where we give information about the class
+    /// </summary>
     public class PeopleController : ApiController
     {
         List<Person> people = new List<Person>();
@@ -16,6 +19,25 @@ namespace DemoAPI.Controllers
             people.Add(new Person { FirstName = "Cersei", Id = 2, LastName = "Lannister" });
             people.Add(new Person { FirstName = "Beni", Id = 3, LastName = "Ibeh" });
         }
+        /// <summary>
+        /// Gets a list of the first names of all users.//more than one get call/ also passes in more than one value
+        /// </summary>
+        /// <param name="userId">The Unique Id for this person</param>
+        /// <param name="age">We want to know how old they are</param>
+        /// <returns>A list of first names</returns>
+
+        [Route("api/People/GetFirstNames/{userId:int}/{age:int}")]
+        [HttpGet]
+        public List<string> GetFirstName(int userId, int age)
+        {
+            List<string> output = new List<string>();
+            foreach (var p in people)
+            {
+                output.Add(p.FirstName);
+            }
+            return output;
+        }
+
         // GET: api/People
         public List<Person> Get()
         {
--- Models/Person.cs
+++ Models/Person.cs
@@ -5,10 +5,22 @@ using System.Web;
 
 namespace DemoAPI.Models
 {
+    /// <summary>
+    /// Class to represent one specific person
+    /// </summary>
     public class Person
     {
+        /// <summary>
+        /// Id from SQL
+        /// </summary>
         public int Id { get; set; } = 0;
+        /// <summary>
+        /// Users firstname
+        /// </summary>
         public string FirstName { get; set; } = "";
+        /// <summary>
+        /// It is obvious what this is
+        /// </summary>
         public string LastName { get; set; } = "";
 
     }
